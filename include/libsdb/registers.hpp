1ab4af7918f7a1673a2257441e3ec9ff7aac655b
danglevm <danglevuminh@gmail.com>
2025-02-11 23:59:56 -0500

    add new functions

--- include/libsdb/registers.hpp
+++ include/libsdb/registers.hpp
@@ -1,8 +1,13 @@
 #ifndef SDB_REGISTERS_HPP
 #define SDB_REGISTERS_HPP
 
+#include "register_info.hpp"
+#include <atomic>
+#include <cstdint>
+#include <variant>
 #include <sys/user.h>
 #include <libsdb/register_info.hpp>
+#include <libsdb/types.hpp>
 
 namespace sdb {
     class process;
@@ -12,6 +17,22 @@ namespace sdb {
     class registers {
         public:
             //using value = /**/;
+            using value = std::variant<std::int8_t, std::int16_t, std::int32_t, std::int64_t,   
+                                        std::uint8_t, std::uint16_t, std::uint32_t, std::uint64_t, 
+                                        float, double, long double,
+                                        sdb::byte64, sdb::byte128>;
+
+            value read(const register_info& info) const;
+            void write(const register_info& info, value val);
+
+            template <typename T>
+            T read_by_id_as(register_id id) const {
+
+            }
+
+            void write_by_id_as(register_id id, value val) const {
+                return write(get_register_info_by_id(id), val);
+            }
         private:
             registers(process& proc) : proc_(&proc){}
             friend process;
